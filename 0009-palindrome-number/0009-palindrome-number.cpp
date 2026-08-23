bad15bb38f6c401f80314aae0a9c954d2337d3e0
Ghost <ankushrawat0154@gmail.com>
2026-08-23 20:36:31 +0530

    Time: 7 ms (13.01%), Space: 8.6 MB (36.14%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        if(x < 0){
+            return 0;
         }
-
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = (reversedHalf * 10) + (x % 10);
-            x /= 10;
+        
+        long long rev = 0; 
+        int y = x, rem = 0;
+        
+        while(x > 0){
+            rem = x % 10;
+            rev = (rev * 10) + rem;
+            x = x / 10;
         }
-
-        return x == reversedHalf || x == reversedHalf / 10;
+        
+        return y == rev;
     }
 };
